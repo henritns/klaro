e685a8dd10475bbd126fadb4aeaa8dc112af5c1a
Henri Tanskanen <henri@kelmu.org>
2020-07-11 23:51:53 +0300

    Starting

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,8 @@
+function logConsent(consent, app) {
+    // This is an example callback function.
+    console.log('User consent for app ' + app.name + ': consent=' + consent);
+}
+
 // By default, Klaro will load the config from  a global "klaroConfig" variable.
 // You can change this by specifying the "data-config" attribute on your
 // script take, e.g. like this:
@@ -27,10 +32,10 @@ var klaroConfig = {
     //cookieDomain: '.github.com',
 
     // Put a link to your privacy policy here (relative or absolute).
-    privacyPolicy: '/#privacy',
+    privacyPolicy: '/#cookienotice',
 
     // Defines the default state for applications (true=enabled by default).
-    default: false,
+    default: true,
 
     // If "mustConsent" is set to true, Klaro will directly display the consent
     // manager modal and not allow the user to close it before having actively
@@ -42,7 +47,7 @@ var klaroConfig = {
     acceptAll: false,
 
     // replace "decline" with cookie manager modal
-    hideDeclineAll: false,
+    hideDeclineAll: true,
 
     // hide "learnMore" link
     hideLearnMore: false,
@@ -63,132 +68,42 @@ var klaroConfig = {
     translations: {
         // If you erase the "consentModal" translations, Klaro will use the
         // bundled translations.
-        de: {
-            consentModal: {
-                description:
-                    'Hier können Sie einsehen und anpassen, welche Information wir über Sie sammeln. Einträge die als "Beispiel" gekennzeichnet sind dienen lediglich zu Demonstrationszwecken und werden nicht wirklich verwendet.',
-            },
-            inlineTracker: {
-                description: 'Beispiel für ein Inline-Tracking Skript',
-            },
-            externalTracker: {
-                description: 'Beispiel für ein externes Tracking Skript',
-            },
-            adsense: {
-                description: 'Anzeigen von Werbeanzeigen (Beispiel)',
-            },
-            matomo: {
-                description: 'Sammeln von Besucherstatistiken',
-            },
-            camera: {
-                description:
-                    'Eine Überwachungskamera (nur ein Beispiel zu IMG-Tags)',
-            },
-            cloudflare: {
-                description: 'Schutz gegen DDoS-Angriffe',
-            },
-            intercom: {
-                description:
-                    'Chat Widget & Sammeln von Besucherstatistiken (nur ein Beispiel)',
-            },
-            mouseflow: {
-                description: 'Echtzeit-Benutzeranalyse (nur ein Beispiel)',
-            },
-            googleFonts: {
-                description: 'Web-Schriftarten von Google gehostet',
-            },
-            purposes: {
-                analytics: 'Besucher-Statistiken',
-                security: 'Sicherheit',
-                livechat: 'Live Chat',
-                advertising: 'Anzeigen von Werbung',
-                styling: 'Styling',
-            },
-        },
         en: {
             consentModal: {
+                title: 'Use of Cookies',
                 description:
-                    'Here you can see and customize the information that we collect about you. Entries marked as "Example" are just for demonstration purposes and are not really used on this website.',
+                    'Here you can see and customize the information that we collect about you with cookies.',
+                privacyPolicy: {
+                    text: 'To learn more, please read our {privacyPolicy}.',
+                    name: 'Cookie Notice',
+                },
             },
-            inlineTracker: {
-                description: 'Example of an inline tracking script',
-            },
-            externalTracker: {
-                description: 'Example of an external tracking script',
-            },
-            adsense: {
-                description: 'Displaying of advertisements (just an example)',
-            },
-            matomo: {
-                description: 'Collecting of visitor statistics',
-            },
-            camera: {
+            consentNotice: {
                 description:
-                    'A surveillance camera (just an example for an IMG tag)',
+                    'We use cookies for the following purposes: {purposes}.',
+                learnMore: 'Customize',
             },
-            cloudflare: {
-                description: 'Protection against DDoS attacks',
-            },
-            intercom: {
+            poweredBy: '',
+            ga: {
                 description:
-                    'Chat widget & collecting of visitor statistics (just an example)',
+                    'Analysis of website audience, retargeting our visitors, advertising reporting',
             },
-            mouseflow: {
-                description: 'Real-Time user analytics (just an example)',
+            facebook: {
+                description:
+                    'Retargeting our visitors on the Facebook platform',
             },
-            googleFonts: {
-                description: 'Web fonts hosted by Google',
+            hubspot: {
+                description: 'Online marketing activities',
+            },
+            essential: {
+                description:
+                    'Cookies we set that are strictly necessary in order for us to provide the service',
             },
             purposes: {
                 analytics: 'Analytics',
                 security: 'Security',
-                livechat: 'Livechat',
+                essential: 'Essential Purposes',
                 advertising: 'Advertising',
-                styling: 'Styling',
-            },
-        },
-        tr: {
-            consentModal: {
-                description:
-                    'Hakkınızda topladığımız bilgileri burada görebilir ve özelleştirebilirsiniz. "Örnek" olarak belirtilenler sadece gösterim amaçlıdır ve gerçekte bu site için kullanılmazlar.',
-            },
-            inlineTracker: {
-                description: 'Satıriçi takip kodu için bir örnek',
-            },
-            externalTracker: {
-                description: 'Dışarıdan çağırılan bir takip kodu için örnek',
-            },
-            adsense: {
-                description: 'Reklam görüntüleme (sadece örnek)',
-            },
-            matomo: {
-                description: 'Ziyaretçi istatistiklerini toplama',
-            },
-            camera: {
-                description:
-                    'İzleme kamerası (IMG tag icin saçma bir örnek daha)',
-            },
-            cloudflare: {
-                description: 'DDoS saldırılarına karşı koruma',
-            },
-            intercom: {
-                description:
-                    'Sohbet aracı ve ziyaretçi istatistiklerini toplama (sadece bir örnek)',
-            },
-            mouseflow: {
-                description:
-                    'Gerçek zamanlı kullanıcı istatistiği (sadece bir örnek)',
-            },
-            googleFonts: {
-                description:
-                    'Google tarafından barındırılan Web Yazıtipi bilgileri',
-            },
-            purposes: {
-                analytics: 'Analitik',
-                security: 'Güvenlik',
-                livechat: 'Canlı Sohbet',
-                advertising: 'Reklam',
-                styling: 'Biçimlendirme',
             },
         },
     },
@@ -196,8 +111,14 @@ var klaroConfig = {
     // This is a list of third-party apps that Klaro will manage for you.
     apps: [
         {
+            name: 'essential',
+            title: 'Essential Cookies',
+            purposes: ['essential', 'security'],
+            required: true,
+        },
+        {
             // Each app should have a unique (and short) name.
-            name: 'matomo',
+            name: 'ga',
 
             // If "default" is set to true, the app will be enabled by default
             // Overwrites global "default" setting.
@@ -206,39 +127,32 @@ var klaroConfig = {
             default: true,
 
             // The title of you app as listed in the consent modal.
-            title: 'Matomo/Piwik',
+            title: 'Google Analytics',
 
             // The purpose(s) of this app. Will be listed on the consent notice.
             // Do not forget to add translations for all purposes you list here.
-            purposes: ['analytics'],
+            purposes: ['analytics', 'advertising'],
 
             // A list of regex expressions or strings giving the names of
             // cookies set by this app. If the user withdraws consent for a
             // given app, Klaro will then automatically delete all matching
             // cookies.
-            cookies: [
-                // you can also explicitly provide a path and a domain for
-                // a given cookie. This is necessary if you have apps that
-                // set cookies for a path that is not "/" or a domain that
-                // is not the current domain. If you do not set these values
-                // properly, the cookie can't be deleted by Klaro
-                // (there is no way to access the path or domain of a cookie in JS)
-                // Notice that it is not possible to delete cookies that were set
-                // on a third-party domain! See the note at mdn:
-                // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#new-cookie_domain
-                [/^_pk_.*$/, '/', 'klaro.kiprotect.com'], //for the production version
-                [/^_pk_.*$/, '/', 'localhost'], //for the local version
-                'piwik_ignore',
-            ],
+            cookies: [/^ga/i],
+            // you can also explicitly provide a path and a domain for
+            // a given cookie. This is necessary if you have apps that
+            // set cookies for a path that is not "/" or a domain that
+            // is not the current domain. If you do not set these values
+            // properly, the cookie can't be deleted by Klaro
+            // (there is no way to access the path or domain of a cookie in JS)
+            // Notice that it is not possible to delete cookies that were set
+            // on a third-party domain! See the note at mdn:
+            // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#new-cookie_domain
 
             // An optional callback function that will be called each time
             // the consent state for the app changes (true=consented). Passes
             // the `app` config as the second parameter as well.
-            callback: function(consent, app) {
-                // This is an example callback function.
-                console.log(
-                    'User consent for app ' + app.name + ': consent=' + consent
-                );
+            callback: function (consent, app) {
+                logConsent(consent, app);
             },
 
             // If "required" is set to true, Klaro will not allow this app to
@@ -257,49 +171,21 @@ var klaroConfig = {
 
         // The apps will appear in the modal in the same order as defined here.
         {
-            name: 'inlineTracker',
-            title: 'Inline Tracker',
-            purposes: ['analytics'],
-            cookies: ['inline-tracker'],
-            optOut: false,
-        },
-        {
-            name: 'externalTracker',
-            title: 'External Tracker',
-            purposes: ['analytics', 'security'],
-            cookies: ['external-tracker'],
-        },
-        {
-            name: 'intercom',
-            title: 'Intercom',
-            default: true,
-            purposes: ['livechat'],
-        },
-        {
-            name: 'mouseflow',
-            title: 'Mouseflow',
-            purposes: ['analytics'],
-        },
-        {
-            name: 'adsense',
-            title: 'Google AdSense',
+            name: 'hubspot',
+            title: 'Hubspot',
             purposes: ['advertising'],
+            default: true,
+            cookies: [
+                [/^_pk_.*$/, '/', 'klaro.kiprotect.com'], //for the production version
+                [/^_pk_.*$/, '/', 'localhost'], //for the local version
+                'piwik_ignore',
+            ],
         },
         {
-            name: 'camera',
-            title: 'Surveillance Camera',
-            purposes: ['security'],
-        },
-        {
-            name: 'googleFonts',
-            title: 'Google Fonts',
-            purposes: ['styling'],
-        },
-        {
-            name: 'cloudflare',
-            title: 'Cloudflare',
-            purposes: ['security'],
-            required: true,
+            name: 'facebook',
+            title: 'Facebook',
+            purposes: ['advertising'],
+            default: true,
         },
     ],
 };
